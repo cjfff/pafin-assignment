b4cc56c91d392ad217e78d114efe2bd2acb2cb92
chenjingfeng <chenjingfeng@shoplineapp.com>
2023-07-31 16:48:11 +0800

    feat: add default port setting and the console shows the infomation about the serser state

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const defaultPort = process.env.PORT || 3000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(defaultPort);
+  console.info(`now is starting http://localhost:${defaultPort}`);
 }
 bootstrap();
